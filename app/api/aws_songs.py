7e1f71abedbfafc524dad1406b61ff8c345f6090
Max K <mkoerner570@gmail.com>
2021-11-08 13:15:35 -0500

    song and image uploader started

---
+++ app/api/aws_songs.py
@@ -0,0 +1,43 @@
+import boto3
+import botocore
+import os
+import uuid
+
+BUCKET_NAME = os.environ.get("S3_BUCKET")
+S3_LOCATION = f"http://{BUCKET_NAME}.s3.amazonaws.com/"
+ALLOWED_EXTENSIONS = {"flac", "wav", "alac", "aiff", "mp3", "mp4"}
+
+s3 = boto3.client(
+   "s3",
+   aws_access_key_id=os.environ.get("S3_KEY"),
+   aws_secret_access_key=os.environ.get("S3_SECRET")
+)
+
+
+def allowed_song(filename):
+    return "." in filename and \
+           filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def get_unique_songname(filename):
+    ext = filename.rsplit(".", 1)[1].lower()
+    unique_filename = uuid.uuid4().hex
+    return f"{unique_filename}.{ext}"
+
+
+def upload_song_to_s3(file, acl="public-read"):
+    try:
+        s3.upload_fileobj(
+            file,
+            BUCKET_NAME,
+            file.filename,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+    except Exception as e:
+        # in case the our s3 upload fails
+        return {"errors": str(e)}
+
+    return {"url": f"{S3_LOCATION}{file.filename}"}
--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
 from app.models.db import Song
 from forms.upload_form import UploadForm
+from api.aws_songs import (
+    upload_song_to_s3, allowed_song, get_unique_songname)
+from api.aws_images import (
+    upload_file_to_s3, allowed_file, get_unique_filename)
 
 song_routes = Blueprint('songs', __name__)
 
@@ -9,15 +13,35 @@ song_routes = Blueprint('songs', __name__)
 @login_required
 def song_post():
     form = UploadForm()
+
+    if "song" not in request.files:
+        return {"errors":"Song required"}, 400
+
+    song = request.files["song"]
+    song.filename = get_unique_songname(song.filename)
+    image = request.files["image"]
+    image.filename = get_unique_filename(image.filename)
+    upload_song = upload_song_to_s3(song)
+    upload_image = upload_file_to_s3
+
+    if "url" not in upload_song or upload_image:
+        return upload_song or upload_image, 400
+
+    url_song = upload_song["url"]
+    url_image = upload_image["url"]
+
     if form.validate_on_submit():
         data = form.data
         new_song = Song(
             title=data["title"],
             artist=data["artist"],
             length=data["length"],
+            song_url = url_song,
+            image_url = url_image
         )
         db.session.add(new_song)
         db.session.commit()
         return redirect("/")
+
     else:
         return "Bad Data"
