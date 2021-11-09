091f374920b1421ca6e51aaf7b94d6f36e3a04e6
Max K <mkoerner570@gmail.com>
2021-11-09 17:18:07 -0500

    working on the forms for upload song

--- react-app/src/components/upload.js
+++ react-app/src/components/upload.js
@@ -6,13 +6,26 @@ import { useSelector } from "react-redux";
 
 function UploadForm({}) {
     const dispatch = useDispatch()
+    const [title,setTitle] = useState("");
+    const [artist,setArtist] = useState("")
+    const [length,setLength] = useState(0)
+    const [selectedSong, setSelectedSong] = useState();
+    const [selectedImage, setSelectedImage] = useState();
+	const [isSongPicked, setIsSongPicked] = useState(false);
+    const [isFilePicked, setIsFilePicked] = useState(false);
     const history = useHistory()
+
+    const changeHandler = (event) => {
+        setSelectedSong(event.target.files[0]);
+		setSelectedImage(event.target.files[0]);
+		setIsSelected(true);
+	};
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        let newNote = dispatch(notesAction.writeNote({ noteBookId, title, contents}))
-        if(newNote){
+        let newSong = dispatch(notesAction.writeNote({ title, artist, length, selectedSong, selectedImage}))
+        if(newSong){
             history.push(`/`)
         }
     }
@@ -31,21 +44,47 @@ function UploadForm({}) {
                 <input
                     id='artist'
                     type="text"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
+                    value={artist}
+                    onChange={(e) => setArtist(e.target.value)}
                     placeholder="Artist name"
                     required
                 />
                 <input
-                    id='title'
+                    id='length'
                     type="number"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
+                    value={length}
+                    onChange={(e) => setLength(e.target.value)}
                     placeholder="Track Length"
                     required
                 />
-                <input type="file" name="Song" onChange={changeHandler} />
-                <input type="file" name="Image" onChange={changeHandler} />
+                <input type="file" name="song" onChange={changeHandler} />
+			    {isSelected ? (
+				    <div>
+					    <p>Filename: {selectedSong.name}</p>
+					    <p>Filetype: {selectedSong.type}</p>
+					    <p>Size in bytes: {selectedSong.size}</p>
+					    <p>
+						    lastModifiedDate:{' '}
+						    {selectedSong.lastModifiedDate.toLocaleDateString()}
+					    </p>
+				    </div>
+			    ) : (
+				    <p>Select a file to show details</p>
+			    )}
+                <input type="file" name="image" onChange={changeHandler} />
+			    {isSelected ? (
+				    <div>
+					    <p>Filename: {selectedFile.name}</p>
+					    <p>Filetype: {selectedFile.type}</p>
+					    <p>Size in bytes: {selectedFile.size}</p>
+					    <p>
+						    lastModifiedDate:{' '}
+						    {selectedFile.lastModifiedDate.toLocaleDateString()}
+					    </p>
+				    </div>
+			    ) : (
+				    <p>Select a file to show details</p>
+			    )}
             </label>
             <button id="submit" type="submit">Submit</button>
         </form>
