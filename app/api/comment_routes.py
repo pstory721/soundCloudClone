e6d8ff7bd82cc14150ad191e46edf269ba801505
Max K <mkoerner570@gmail.com>
2021-11-09 12:10:16 -0500

    access to current user info found. Work begun on other CRUD routes

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -5,13 +5,17 @@ from app.forms.comment_form import CommentForm
 
 comment_routes = Blueprint('comments', __name__)
 
+
+# Posts a new comment to the song
 @comment_routes.route("/comments",methods=["POST"])
 @login_required
 def post_comment():
     form = CommentForm()
+    user = current_user.id
     if form.validate_on_submit():
         data = form.data
         new_comment = Comments(
+            user_id = user,
             content = data["content"]
         )
         db.session.add(new_comment)
@@ -20,7 +24,13 @@ def post_comment():
     else:
         return "Bad Data"
 
+
+# Delete's a comment made by the user
 @comment_routes.route('/comments/:id',methods=["DELETE"])
 @login_required
 def delete_comment():
-    Comments.query.filter(Comments.id == id).delete()
+    current_comment = Commnets["id"]
+    if current_comment["user_id"] not current_user.id:
+        return "Cannot complete request", 403
+    db.session.delete(current_comment)
+    return redirect("/")
