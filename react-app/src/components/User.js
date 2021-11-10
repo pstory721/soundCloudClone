ea70c1740224b6111f160e7cb6d86d355af6127c
sam <ortegachan867@gmail.com>
2021-11-10 12:44:42 -0500

    forgot to merge my stuffs

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { GetAllSongs } from "../store/song";
 import './user.css';
 
 function User() {
