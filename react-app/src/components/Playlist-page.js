59277021dceffabea136581b28c88b2962f30211
patrick story <pstory721@gmail.com>
2021-11-11 17:25:10 -0600

    lol peters a cool dude

--- react-app/src/components/Playlist-page.js
+++ react-app/src/components/Playlist-page.js
@@ -1,6 +1,6 @@
 import React, {  useEffect, useState }  from "react";
 import { useDispatch, useSelector } from "react-redux";
-import GetAllPlaylist from '../store/playlist';
+import {GetAllPlaylist} from '../store/playlist';
 import './Playlist-page.css';
 
 
