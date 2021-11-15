5009ba4c0f67bf09e64d6f264646660b54b3e5ab
sam <ortegachan867@gmail.com>
2021-11-14 20:43:46 -0500

    seeding

--- app/seeds/whatever.py
+++ app/seeds/whatever.py
@@ -63,6 +63,46 @@ def seed_songs():
         title='DayWalker', artist ='MGK FT.Corpse', length = 2.19, likes = 36, song_url ="https://res.cloudinary.com/dveuedvvm/video/upload/v1636840449/Machine_Gun_Kelly_feat._CORPSE_-_DAYWALKER_Official_Audio_msiunt.mp4",
         image_url = "https://res.cloudinary.com/dveuedvvm/image/upload/v1636840868/machinegunkellyvideo_mdbwnz.jpg", user_id = 4
     )
+    Venom = Song(
+        title='Venom', artist = 'GHOSTEMANE', length = 2.06, likes = 20, song_url = 'https://res.cloudinary.com/dzjkwepju/video/upload/v1636938882/Styckr/GHOSTEMANE_-_VENOM_s4fg7q.mp4', image_url = 'https://res.cloudinary.com/dzjkwepju/image/upload/v1636939164/Styckr/91YhSqj7PUL._SS500__gixlaj.jpg',
+        user_id = 2
+    )
+    Mercury = Song(
+        title='Mercury', artist = 'GHOSTEMANE', length = 2.05, likes = 38, song_url = 'https://res.cloudinary.com/dzjkwepju/video/upload/v1636938876/Styckr/GHOSTEMANE_-_Mercury_ovxgyo.mp4', image_url = 'https://res.cloudinary.com/dzjkwepju/image/upload/v1636939312/Styckr/34dcc3c1bd9510414b6d321af0bd1d1b_da1g8r.jpg',
+        user_id = 2
+    )
+    Nihil = Song(
+        title='Nihil', artist = 'GHOSTEMANE', length = 2.48, likes = 41, song_url = 'https://res.cloudinary.com/dzjkwepju/video/upload/v1636938878/Styckr/GHOSTEMANE_-_Nihil_rwmvmw.mp4', image_url = 'https://res.cloudinary.com/dzjkwepju/image/upload/v1636939408/Styckr/artworks-000417587730-zqc3hf-t500x500_a9rg1v.jpg',
+        user_id = 2
+    )
+    Andromeda = Song(
+        title = 'Andromeda', artist = 'GHOSTEMANE', length = 2.07, likes = 16, song_url = 'https://res.cloudinary.com/dzjkwepju/video/upload/v1636938873/Styckr/GHOSTEMANE_-_Andromeda_Official_Video_b094mp.mp4', image_url = 'https://res.cloudinary.com/dzjkwepju/image/upload/v1636939528/Styckr/819biVRjfrL._SS500__oy5qgp.jpg',
+        user_id = 2
+    )
+    KYOTO = Song(
+        title = 'KYOTO', artist='Skrillex', length = 3.20, likes = 79, song_url = 'https://res.cloudinary.com/dzjkwepju/video/upload/v1636938887/Styckr/SKRILLEX_-_KYOTO_FT._SIRAH_t3v9m9.mp4', image_url = 'https://res.cloudinary.com/dzjkwepju/image/upload/v1636939651/Styckr/220px-Skrillex_-_Bangarang__EP_mno0cb.png',
+        user_id = 3
+    )
+    First_Of_The_Year_Equinox = Song(
+        title= 'First of the Year Exquinox', artist = 'Skrillex', length = 3.14, likes = 43, song_url = 'https://res.cloudinary.com/dzjkwepju/video/upload/v1636938885/Styckr/Skrillex_-_First_Of_The_Year_Equinox_Official_Music_Video_dfpmff.mp4', image_url = 'https://res.cloudinary.com/dzjkwepju/image/upload/v1636939806/Styckr/artworks-000010757559-s028go-t500x500_ycshiw.jpg',
+        user_id = 3
+    )
+    PA_PA_YA = Song(
+        title = 'PA PA YA', artist = 'BABYMETAL', length = 3.55, likes = 13, song_url = 'https://res.cloudinary.com/dzjkwepju/video/upload/v1636938866/Styckr/BABYMETAL_-_PA_PA_YA_feat._F.HERO_OFFICIAL_kgcqb3.mp4', image_url = 'https://res.cloudinary.com/dzjkwepju/image/upload/v1636939944/Styckr/220px-Babymetal_Pa_Pa_Ya__digital_single_cover_hod5hl.jpg',
+        user_id = 2
+    )
+    Squeeze = Song(
+        title = 'Squeeze', artist ='GHOSTEMANE', length = 3.14, likes = 62, song_url = 'https://res.cloudinary.com/dzjkwepju/video/upload/v1636938880/Styckr/GHOSTEMANE_-_Squeeze_eetwjy.mp4', image_urn = 'https://res.cloudinary.com/dzjkwepju/image/upload/v1636940045/Styckr/71DYG_Ft5-L._AC_UL600_SR600_600__edqpxv.jpg',
+        user_id = 2
+    )
+    Duality = Song(
+        title = 'Duality', artist ='Slipknot', length = 3.33, likes = 99, song_url = 'https://res.cloudinary.com/dzjkwepju/video/upload/v1636938891/Styckr/Slipknot_-_Duality_OFFICIAL_VIDEO_ynu5u5.mp4', image_url = 'https://res.cloudinary.com/dzjkwepju/image/upload/v1636940152/Styckr/R-581973-1345822174-7830.jpeg_ja37i6.jpg',
+        user_id = 4
+    )
+    Before_I_Forget = Song(
+        title= 'Before I Forget', artist='Slipknot', length = 4.01, likes = 71, song_url = 'https://res.cloudinary.com/dzjkwepju/video/upload/v1636938889/Styckr/Slipknot_-_Before_I_Forget_OFFICIAL_VIDEO_t6czbu.mp4', image_url = 'https://res.cloudinary.com/dzjkwepju/image/upload/v1636940266/Styckr/13b50ed3d58945178c9f0b6767bd2feb_nkkn7m.jpg',
+        user_id = 4
+    )
 
     db.session.add(Epic)
     db.session.add(Carolltan)
@@ -73,5 +113,15 @@ def seed_songs():
     db.session.add(Popular)
     db.session.add(Wash)
     db.session.add(Day)
+    db.session.add(Venom)
+    db.session.add(Mercury)
+    db.session.add(Nihil)
+    db.session.add(Andromeda)
+    db.session.add(KYOTO)
+    db.session.add(First_Of_The_Year_Equinox)
+    db.session.add(PA_PA_YA)
+    db.session.add(Squeeze)
+    db.session.add(Duality)
+    db.session.add(Before_I_Forget)
 
     db.session.commit()
