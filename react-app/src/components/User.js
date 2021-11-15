be4df48771eb08ec39dc810fdf914762841fbb0b
sam <ortegachan867@gmail.com>
2021-11-14 23:46:17 -0500

    too much debugging

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -32,16 +32,13 @@ function User() {
 
   function toggle_visibility(id, id2) {
     let something = document.getElementById(id);
-    let somethingElse = document.getElementById(id2);
-    if(something.style.display === 'block' && somethingElse.style.display === 'none'){
+    if(something.style.display === 'block'){
       something.style.display = 'none';
-      somethingElse.style.display = 'block';
 
     }
     else{
 
       something.style.display = 'block';
-      somethingElse.style.display = 'none';
     }
  }
 
@@ -56,7 +53,7 @@ function User() {
               All
             </NavLink>
               <div className='all-songs' id="song">
-              {songs?.map((song) => (
+              {songs?.filter(song=> song.user_id === sessionUser.id).map((song) => (
               <div className='shiz-giggles' >
                 <ul>
                 <li className='li-images'><img className='user-all' src={`${song.image_url}`} alt="ooops that broke"></img></li>
@@ -65,14 +62,14 @@ function User() {
               </div>
           </li>
           {/* <li className='lis'><NavLink className='lis-text' to={`/users/${sessionUser.id}`}>Likes</NavLink></li> */}
-          <li className='lis'>
+          {/* <li className='lis'>
             <NavLink className='lis-text' to={`/users/${sessionUser.id}`} onClick={()=> toggle_visibility('play', 'song')}>
               Playlist
             </NavLink >
               <div className='all-playlist' id="play">
                 <RealPlaylist/>
               </div>
-          </li>
+          </li> */}
           {/* <li className='lis'><NavLink className='lis-text' to={`/users/${sessionUser.id}`}>History</NavLink></li> */}
       </ul>
 
